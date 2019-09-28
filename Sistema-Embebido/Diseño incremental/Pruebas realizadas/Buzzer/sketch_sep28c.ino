834f2147377a797a3ed781ced499e471fa6ed2d4
tomascastillo <castillotomas96@gmail.com>
2019-09-28 12:18:07 -0300

    Agrego diagramas

---
+++ Sistema-Embebido/Diseño incremental/Pruebas realizadas/Buzzer/sketch_sep28c.ino
@@ -0,0 +1,12 @@
+const int pin = 9;
+ 
+void setup() {
+  pinMode(pin, OUTPUT);  //definir pin como salida
+}
+ 
+void loop(){
+  digitalWrite(pin, HIGH);   // poner el Pin en HIGH
+  delay(500);               // esperar 5 segundos
+  digitalWrite(pin, LOW);    // poner el Pin en LOW
+  delay(200);               // esperar 20 segundos
+}
